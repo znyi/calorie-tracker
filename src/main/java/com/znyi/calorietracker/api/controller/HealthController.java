6ba093ab549018d7a9ea5156093a64f6c285d1d3
David Arustamyan <arustdav@fel.cvut.cz>
2025-08-10 23:37:51 +0200

    Add /health endpoint that returns "ok"

---
+++ src/main/java/com/znyi/calorietracker/api/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.znyi.calorietracker.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthChecked() {
+        return "ok";
+    }
+}
