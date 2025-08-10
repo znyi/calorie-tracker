09d9f4af9e0f36f13b93240bd1ca90145552229d
znyi <xinyi2709@gmail.com>
2025-08-10 20:10:33 +0900

    chore: add initial project

---
+++ src/test/java/com/znyi/calorietracker/CalorieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znyi.calorietracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
